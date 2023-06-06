91b4cfc444e65159c44c6e782f284ef7e9de64ed
Muhammad <sirmoohfabrics@gmail.com>
2023-06-06 09:08:20 -0700

    task 3 changed

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z') + 1):
-    if chr(letter) not in ['q', 'e']:
-        print(chr(letter), end='')
+for m in range(97, 123):
+    if m != 101 and m != 113:
+        print(chr(m), end='')
