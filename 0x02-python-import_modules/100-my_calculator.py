fb277f54f56dfb3d44d32a4784f1addbca818521
Muhammad <sirmoohfabrics@gmail.com>
2023-06-08 09:22:32 -0700

    task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    """
+    Handle basic arithmetic operations.
+    """
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if not any(sys.argv[2] == operator for operator in ops):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
