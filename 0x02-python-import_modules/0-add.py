44e2255c4282939dc03d23b2c91d3c8679234cd9
Muhammad <sirmoohfabrics@gmail.com>
2023-06-08 08:35:22 -0700

    changed

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import add_0
-
 if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    z = add_0.add(a, b)
+    z = add(a, b)
     print("{} + {} = {}".format(a, b, z))
