c0e61af64460c93d0ed4da5522a57389001a4693
Muhammad <sirmoohfabrics@gmail.com>
2023-06-08 09:43:57 -0700

    task 8

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/bin/usr/bin/ptyhon3
+
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if (a < b):
+        c = add(a, b)
+        for p in range(4, 6):
+            y = add(y, p)
+        return(y)
+    return(sub(a, b))
