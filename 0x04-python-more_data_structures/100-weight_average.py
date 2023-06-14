f2d85f8726b3724267f0b106b6988ef80a603289
Muhammad <sirmoohfabrics@gmail.com>
2023-06-14 06:55:51 -0700

    task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,6 @@
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    sum_score = sum(score * weight for score, weight in my_list)
+    sum_weight = sum(weight for _, weight in my_list)
+    return sum_score / sum_weight
